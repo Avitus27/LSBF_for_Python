cd16330fdfc882c322ea69b536179459a2b41c95
Avitus27 <mervyn511@gmail.com>
2017-03-27 21:09:56 +0100

    Add initializer for Bloom Array

--- LSBF.py
+++ LSBF.py
@@ -1,7 +1,20 @@
 import hashlib
+import math
+
+from bitarray import bitarray
 
 def getArrayPos( input ):
 	return ( int(bin(int(input, 16))[2:].zfill(8)[0:16], 2), int(bin(int(input, 16))[2:].zfill(8)[16:32], 2) )
+
+def setArrayBit( outerArray, bitPosition ):
+        return True
+
+emptyArray = bitarray(2**16)
+bloomArray = [None]
+
+for count in xrange( 65536 ):
+	bloomArray.append(emptyArray)
+
 
 #hashlib.md5('a'.encode('utf-8'))
 #hashlib.sha512('a')
