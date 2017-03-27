ca136f1fd4379c1f3f8a98819dde71bc53ecba86
Avitus27 <mervyn511@gmail.com>
2017-03-27 20:51:47 +0100

    Incremental commit

--- LSBF.py
+++ LSBF.py
@@ -1,7 +1,42 @@
-from pybloom import BloomFilter
+import hashlib
 
-bf = BloomFilter(capacity=10000, error_rate=0.0001)
+def getArrayPos( input ):
+	return ( int(bin(int(input, 16))[2:].zfill(8)[0:16], 2), int(bin(int(input, 16))[2:].zfill(8)[16:32], 2) )
 
-[bf.add(x) for x in range(10)]
+#hashlib.md5('a'.encode('utf-8'))
+#hashlib.sha512('a')
+
+#hashlib.md5('a'.encode('utf-8')).digest()
+
+#print hashlib.sha512('a').hexdigest()
+
+m5_a = hashlib.md5()
+
+m5_a.update('a'.encode('utf-8'))
+
+hex = m5_a.hexdigest()
+binary = m5_a.digest()
+
+#print(hex)
+#print(hex[0:16])
+#print(hex[16:32])
 
 
+#print(len(binary))
+#print(binary)
+#print(binary[0:8])
+#print(binary[8:16])
+
+#print(len(bin(int(hex, 16))[2:].zfill(8)))
+
+#print(bin(int(hex, 16)))
+print("decimal here:")
+print(int(bin(int(hex, 16))[2:].zfill(8)[0:16], 2))
+print(int(bin(int(hex, 16))[2:].zfill(8)[16:32], 2))
+
+print("test function:")
+print(getArrayPos(hex))
+
+myVar = getArrayPos(hex)
+
+print(myVar[1])
