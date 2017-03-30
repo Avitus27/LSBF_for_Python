f0e118eed86445dd4fe6b1919386ab1cfb6d63ed
Avitus27 <mervyn511@gmail.com>
2017-03-30 14:09:08 +0100

    small fix for #1

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import LSBF
 def testFunc():
     myBloom = LSBF.LocalitySensitiveBloomFilter( 2, 2, 10, 0.5 )
     print("Should be False: " + str(myBloom.checkInBloom(5)))
-    myBloom.addToBloom(myBloom, 5)
+    myBloom.addToBloom(5)
     print("Should be True: " + str(myBloom.checkInBloom(5)))
     print("Should be True: " + str(myBloom.checkInBloom(5.000)))
     print("Should be True: " + str(myBloom.localityBloomCheck(3.0000)))
